07da73f0732a3c67c9d90138fbf32f86d86e2427
hardygrunter <ih8tw8@gmail.com>
2022-01-23 19:53:18 +0200

    sort movieDB.movies create movieList based on resulting array add movie numbering

--- project2/js/script.js
+++ project2/js/script.js
@@ -15,20 +15,34 @@
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против..."
+  ]
 };
 
 const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre');
+  poster = document.querySelector('.promo__bg'),
+  genre = poster.querySelector('.promo__genre'),
+  movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(img => img.remove());
 
 genre.textContent = 'драма';
+
 poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies
+  .sort()
+  .forEach((movie, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${movie}
+      <div class="delete"></div>
+    </li>
+    `;
+  });
