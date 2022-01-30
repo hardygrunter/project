24dfde0788364993f699775819afd8e06a8550be
hardygrunter <ih8tw8@gmail.com>
2022-01-30 15:45:07 +0200

    add animated tabs

---
+++ project3/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParrent = document.querySelector('.tabheader__items');
+
+  const hideTabContent = () => {
+    tabsContent.forEach(tab => {
+      tab.classList.add('hide');
+      tab.classList.remove('show', 'fade');
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  };
+  const showTabContent = (i = 0) => {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  };
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParrent.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((tab, i) => {
+        if (tab === target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
