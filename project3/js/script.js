23fb572a0de84ff1fcf83a17d1a00e06b7eb703b
hardygrunter <ih8tw8@gmail.com>
2022-01-30 22:38:42 +0200

    added promotion timer

--- project3/js/script.js
+++ project3/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
+
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParrent = document.querySelector('.tabheader__items');
@@ -34,4 +36,53 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  // Timer
+  const deadline = '2022-01-31';
+
+  function getTimeRemaining(endTime) {
+    const diff = Date.parse(endTime) - Date.now(),
+          days = Math.floor(diff / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(diff / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(diff / (1000 * 60) % 60),
+          seconds = Math.floor(diff / 1000 % 60);
+
+    return {
+      total: diff,
+      days,
+      hours,
+      minutes,
+      seconds
+    }; 
+  }
+
+  function getZero(num) {
+    return num <= 0 || isNaN(num) ? '0' : num < 10 ? `0${num}` : `${num}`;
+  }
+
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const remainingTime = getTimeRemaining(endTime);
+
+      if (remainingTime <= 0) {
+        clearInterval(timeInterval);
+      }
+
+      days.textContent = getZero(remainingTime.days);
+      hours.textContent = getZero(remainingTime.hours);
+      minutes.textContent = getZero(remainingTime.minutes);
+      seconds.textContent = getZero(remainingTime.seconds);
+    }
+  }
+
+  setClock('.timer', deadline);
 });
