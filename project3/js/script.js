4a57d010e60ca3f1579563d9efd0119a2232eedd
hardygrunter <ih8tw8@gmail.com>
2022-02-06 20:08:22 +0200

    refactoring menu cards

--- project3/js/script.js
+++ project3/js/script.js
@@ -146,13 +146,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Cards
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
       this.transfer = 27;
+      this.classes = classes.length ? classes : ['menu__item'];
       this.parent = document.querySelector(parentSelector);
       this.changeToUAH();
     }
@@ -163,16 +164,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const elem = document.createElement('div');
+      this.classes.forEach(className => elem.classList.add(className));
       elem.innerHTML = `
-        <div class="menu__item">
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-              <div class="menu__item-cost">Цена:</div>
-              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-          </div>
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
       `;
       this.parent.append(elem);
@@ -194,7 +194,8 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     20.37,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 
   new MenuCard(
@@ -203,6 +204,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     15.92,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 });
